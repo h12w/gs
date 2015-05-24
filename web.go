4c7e33c9024b4b95ad04d6470b89c723c5c9798a
Hǎiliàng Wáng <w@h12.me>
2015-05-24 22:27:25 +0800

    make Proxy setting fluent.

--- web.go
+++ web.go
@@ -12,20 +12,22 @@ import (
 )
 
 type HTTP struct {
-	Proxy  string
-	client *http.Client
+	proxy  string
+	client http.Client
+}
+
+func (h HTTP) Proxy(proxy string) HTTP {
+	h.proxy = proxy
+	u, err := url.Parse(h.proxy)
+	c(err)
+	switch u.Scheme {
+	case "socks5":
+		h.client.Transport = &http.Transport{Dial: socks.DialSocksProxy(socks.SOCKS5, u.Host)}
+	}
+	return h
 }
 
 func (h HTTP) Get(uri string) WebPage {
-	if h.client == nil {
-		h.client = &http.Client{}
-		u, err := url.Parse(h.Proxy)
-		c(err)
-		switch u.Scheme {
-		case "socks5":
-			h.client.Transport = &http.Transport{Dial: socks.DialSocksProxy(socks.SOCKS5, u.Host)}
-		}
-	}
 	resp, err := h.client.Get(uri)
 	for i := 0; i < 3; i++ {
 		resp, err = h.client.Get(uri)
@@ -46,16 +48,13 @@ type WebPage struct {
 	err  error
 }
 
-func (p WebPage) Save(file string) error {
-	if p.err != nil {
-		return p.err
-	}
+func (p WebPage) Save(file string) {
+	c(p.err)
 	f, err := os.Create(file)
 	c(err)
 	defer f.Close()
 	_, err = f.Write(p.body)
 	c(err)
-	return nil
 }
 
 func (p WebPage) Parse() *query.Node {
