7fbed872381c4d8315ba92d2939f4e13e759d151
Hǎiliàng Wáng <w@h12.me>
2016-07-05 19:32:37 +0800

    Add SaveFile

--- file.go
+++ file.go
@@ -8,3 +8,10 @@ func PathExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
 }
+
+func SaveFile(file, s string) {
+	f, err := os.Create(file)
+	c(err)
+	defer f.Close()
+	f.Write([]byte(s))
+}
